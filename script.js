49c6cd3d2083622d690d0e9b30571408e99d5c37
QuentinRiv <32091703+QuentinRiv@users.noreply.github.com>
2020-07-14 12:22:02 +0200

    Add script

---
+++ script.js
@@ -0,0 +1,227 @@
+
+
+id = '2'
+
+var path = "";
+var fetch_path = "";
+// id = Math.floor(Math.random() * 5).toString();
+var path_display = "";
+
+
+local = true
+if (local) {
+    path = `test/` + id;
+    path_display = 'display/' + id;
+    fetch_path = `${window.origin}` + '/';
+}
+else {
+    path = `https://retry-unige.herokuapp.com/test/` + id;
+    path_display = `https://retry-unige.herokuapp.com/display/` + id;
+    fetch_path = `https://retry-unige.herokuapp.com/`;
+}
+
+var allWords = [];
+
+async function getUserAsync(path) {
+    let response = await fetch(path, {
+        // fetch(`https://retry-unige.herokuapp.com/addDB`, {
+        method: "GET",
+        credentials: "include",
+        cache: "no-cache",
+        headers: new Headers({
+            "content-type": "application/json",
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        })
+    })
+    let data = await response.json();
+    return data;
+}
+
+
+async function prepareWords(path) {
+    var fin = await getUserAsync(path);
+    var allWords = fin['words'];
+    $('#container').append(`<table>`)
+    for (var value of allWords) {
+        $('#container').append(`<tr>`)
+        $('#container').append(`<td><input type="checkbox" id="${value}" name="interest" value="${value}"></td>`)    // Rajoute checkbox
+        $('#container').append(`<td><label for="${value}">${value}</label></td>`)                              // Associe le nom
+        $('#container').append(`<td><button class="flag" value="${value}"><i class="fa fa-flag"></i></button></td>`) // Met le bouton principal
+        // $('#container').append(`<br>`);
+        $('#container').append(`</tr>`)
+    }
+    $('#container').append(`</table>`)
+    $('#imElem').attr("src", fin["path"]);          // Source de l'image a affiché
+    flaggedWords = [];
+
+    $('#ima').attr("src", fin["path"]);
+    $('#a_ima').attr("href", fin["path"]);
+
+    return fin
+}
+
+
+
+
+
+
+prepareWords(path);
+
+
+
+// Fonction qui, quand on appuie sur le drapeau, rajoute le mot flaggé dans une liste
+$(document).on("click", ".flag", function () {
+    flaggedWords.push($(this).attr("value"));
+});
+
+
+// Pour quand on confirme les choix de mots sélectionnés
+async function submit_message() {
+    console.debug("In Submit");
+    var result = await getUserAsync(path);
+    allWords = result["words"];
+    var selectedWords = [];
+    for (var value of allWords) {
+        var button = document.getElementById(value);
+        if (button.checked) {
+            selectedWords.push(button.value)
+        }
+    }
+    var textfield = document.getElementById("autre_valeur");
+
+    var entry = {
+        id: id,
+        selwords: selectedWords,
+        flagwords: flaggedWords,
+        newwords : textfield.value
+    };
+
+
+    fetch(fetch_path + 'addDB', {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(entry),
+        cache: "no-cache",
+        headers: new Headers({
+            "content-type": "application/json"
+        })
+    })
+        .then(function (response) {
+            if (response.status !== 200) {
+                console.log(`Looks like there was a problem. Status code: ${response.status}`);
+                return;
+            }
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        })
+        .catch(function (error) {
+            console.log("Fetch error: " + error);
+        });
+
+}
+
+
+async function delete_message() {
+    var result = await getUserAsync(path);
+
+    allWords = result["words"];
+    var selectedWords = [];
+
+    for (var value of allWords) {
+        var button = document.getElementById(value);
+        if (button.checked) {
+            selectedWords.push(button.value)
+        }
+    }
+
+    var entry = {
+        id: id,
+        selwords: selectedWords,
+    };
+
+    fetch(fetch_path + 'delete', {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(entry),
+        cache: "no-cache",
+        headers: new Headers({
+            "content-type": "application/json"
+        })
+    })
+        .then(function (response) {
+            console.log('Response:', response)
+            if (response.status !== 200) {
+                console.log(`Looks like there was a problem. Status code: ${response.status}`);
+                return;
+            }
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        })
+        .catch(function (error) {
+            console.log("Fetch error: " + error);
+        });
+
+}
+
+
+$(function () {
+    $('.demo_fluid a').fluidbox();
+});
+
+
+var randomScalingFactor = function () {
+    return Math.round(Math.random() * 100);
+};
+
+var config = {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [1, 2, 3, 4, 5],
+            backgroundColor: [
+                "red",
+                "orange",
+                "yellow",
+                "green",
+                "blue",
+            ],
+            label: 'Dataset 1'
+        }],
+        labels: [
+            'Red',
+            'Orange',
+            'Yellow',
+            'Green',
+            'Blue'
+        ]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Donut'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        }
+    }
+};
+
+window.onload = async function () {
+    var ctx = document.getElementById('chart-area').getContext('2d');
+    var data = await getUserAsync(path_display)
+    var couleur = ['blue', 'green', 'red', 'grey', 'purple', 'brown', 'cyan', 'yellow']
+    var sizeWords = data['words'].length;
+
+    config['data']['datasets'][0]['data'] = data['count'];
+    config['data']['datasets'][0]['backgroundColor'] = couleur.slice(0, sizeWords);
+    config['data']['labels'] = data['words'];
+    window.myDoughnut = new Chart(ctx, config);
+};
